1aea6253d406e3d98e723e0dc55a2f5c55d79fa3
justcosmic <bartlomiej345@gmail.com>
2020-09-28 21:45:59 +0200

    Attempt to organize code

---
+++ src/image.zig
@@ -0,0 +1,58 @@
+const c = @cImport({
+    @cInclude("FreeImage.h");
+});
+
+pub const Image = struct {
+    bitmap: *c.FIBITMAP,
+    format: c.FREE_IMAGE_FORMAT,
+
+    pub fn unload(self: Image) void {
+        c.FreeImage_Unload(self.bitmap);
+    }
+
+    pub fn getWidth(self: Image) c_uint {
+        return c.FreeImage_GetWidth(self.bitmap);
+    }
+
+    pub fn getHeight(self: Image) c_uint {
+        return c.FreeImage_GetHeight(self.bitmap);
+    }
+
+    pub fn getBits(self: Image) *u8 {
+        return c.FreeImage_GetBits(self.bitmap);
+    }
+
+    pub fn getRedMask(self: Image) c_uint {
+        return c.FreeImage_GetRedMask(self.bitmap);
+    }
+
+    pub fn getBlueMask(self: Image) c_uint {
+        return c.FreeImage_GetBlueMask(self.bitmap);
+    }
+
+    pub fn getGreenMask(self: Image) c_uint {
+        return c.FreeImage_GetGreenMask(self.bitmap);
+    }
+
+    pub fn getBPP(self: Image) c_uint {
+        return c.FreeImage_GetBPP(self.bitmap);
+    }
+
+    pub fn getPitch(self: Image) c_uint {
+        return c.FreeImage_GetPitch(self.bitmap);
+    }
+};
+
+pub fn loadImage(path: [*:0]const u8) !Image {
+    const format = c.FreeImage_GetFileType(path, 0);
+    const bitmap = c.FreeImage_Load(format, path, 0) orelse return error.ImageOpenError;
+
+    return Image{
+        .bitmap = bitmap,
+        .format = format,
+    };
+}
+
+pub fn saveImage(image: Image, path: [*:0]const u8) void {
+    _ = c.FreeImage_Save(image.format, image.bitmap, path, 0);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,21 @@
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
-    @cInclude("FreeImage.h");
 });
 const std = @import("std");
 
-const Image = struct {
-    bitmap: *c.FIBITMAP,
-    format: c.FREE_IMAGE_FORMAT,
-};
+const img = @import("image.zig");
+const Image = img.Image;
 
-fn loadImage(path: [*:0]const u8) !Image {
-    const format = c.FreeImage_GetFileType(path, 0);
-    const bitmap = c.FreeImage_Load(format, path, 0) orelse return error.ImageOpenError;
-
-    return Image{
-        .bitmap = bitmap,
-        .format = format,
-    };
-}
-
-fn createTextureFromBitmap(renderer: *c.SDL_Renderer, bitmap: *c.FIBITMAP) !?*c.SDL_Texture {
+fn createTextureFromBitmap(renderer: *c.SDL_Renderer, image: Image) !*c.SDL_Texture {
     const surface = c.SDL_CreateRGBSurfaceFrom(
-        c.FreeImage_GetBits(bitmap),
-        @intCast(c_int, c.FreeImage_GetWidth(bitmap)),
-        @intCast(c_int, c.FreeImage_GetHeight(bitmap)),
-        @intCast(c_int, c.FreeImage_GetBPP(bitmap)),
-        @intCast(c_int, c.FreeImage_GetPitch(bitmap)),
-        c.FreeImage_GetRedMask(bitmap),
-        c.FreeImage_GetGreenMask(bitmap),
-        c.FreeImage_GetBlueMask(bitmap),
+        image.getBits(),
+        @intCast(c_int, image.getWidth()),
+        @intCast(c_int, image.getHeight()),
+        @intCast(c_int, image.getBPP()),
+        @intCast(c_int, image.getPitch()),
+        image.getRedMask(),
+        image.getGreenMask(),
+        image.getBlueMask(),
         0x000000ff,
     );
     defer c.SDL_FreeSurface(surface);
@@ -36,69 +23,86 @@ fn createTextureFromBitmap(renderer: *c.SDL_Renderer, bitmap: *c.FIBITMAP) !?*c.
     return c.SDL_CreateTextureFromSurface(renderer, surface) orelse error.TextureCreationFailed;
 }
 
+const App = struct {
+    window: *c.SDL_Window,
+    renderer: *c.SDL_Renderer,
+    running: bool = true,
+    image: Image,
+    texture: *c.SDL_Texture,
+
+    pub fn saveImage(self: *App) void {
+        img.saveImage(self.image, "output_image");
+    }
+
+    pub fn handleEvents(self: *App) void {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+                c.SDL_QUIT => self.running = false,
+                c.SDL_WINDOWEVENT => switch (event.window.event) {
+                    c.SDL_WINDOWEVENT_RESIZED => {
+                        std.debug.warn(
+                            "Window {} resized to {}x{}\n",
+                            .{ event.window.windowID, event.window.data1, event.window.data2 },
+                        );
+                    },
+                    else => {},
+                },
+                else => {},
+            }
+        }
+    }
+
+    pub fn draw(self: *App) void {
+        _ = c.SDL_RenderClear(self.renderer);
+        _ = c.SDL_RenderCopyEx(self.renderer, self.texture, null, null, 0, null, @intToEnum(c.SDL_RendererFlip, c.SDL_FLIP_VERTICAL));
+        _ = c.SDL_RenderPresent(self.renderer);
+    }
+};
+
 pub fn main() anyerror!void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
         c.SDL_Log("Failed to initialize SDL: %s", c.SDL_GetError());
-        return;
+        return error.InitFailed;
     }
     defer c.SDL_Quit();
 
     const window = c.SDL_CreateWindow("test", 0, 0, 800, 800, c.SDL_WINDOW_RESIZABLE) orelse {
         c.SDL_Log("Failed to create window: %s", c.SDL_GetError());
-        return;
+        return error.InitFailed;
     };
     defer c.SDL_DestroyWindow(window);
 
     const renderer = c.SDL_CreateRenderer(window, -1, 0) orelse {
         c.SDL_Log("Failed to create renderer: %s", c.SDL_GetError());
-        return;
+        return error.InitFailed;
     };
     defer c.SDL_DestroyRenderer(renderer);
 
-    // This is not needed when using FreeImage as a .SO
-    // c.FreeImage_Initialise(0);
-    // defer c.FreeImage_DeInitialise();
-
     const path = "zig-cache/bin/test.jpg";
-    const image = loadImage(path) catch {
+    const image = img.loadImage(path) catch {
         std.debug.warn("Failed opening {}\n", .{path});
-        return;
+        return error.InitFailed;
     };
-    defer c.FreeImage_Unload(image.bitmap);
+    defer image.unload();
 
-    const texture = createTextureFromBitmap(renderer, image.bitmap) catch {
+    const texture = createTextureFromBitmap(renderer, image) catch {
         std.debug.warn("Failed to create texture\n", .{});
-        return;
+        return error.InitFailed;
     };
     defer c.SDL_DestroyTexture(texture);
 
-    var open = true;
-    while (open) {
-        var event: c.SDL_Event = undefined;
-        while (c.SDL_PollEvent(&event) != 0) {
-            switch (event.type) {
-                c.SDL_QUIT => {
-                    open = false;
-                },
-                c.SDL_WINDOWEVENT => {
-                    switch (event.window.event) {
-                        c.SDL_WINDOWEVENT_RESIZED => {
-                            std.debug.warn(
-                                "Window {} resized to {}x{}\n",
-                                .{ event.window.windowID, event.window.data1, event.window.data2 },
-                            );
-                        },
-                        else => {},
-                    }
-                },
-                else => {},
-            }
-        }
+    var app = App{
+        .window = window,
+        .renderer = renderer,
+        .image = image,
+        .texture = texture,
+    };
 
-        _ = c.SDL_RenderClear(renderer);
-        _ = c.SDL_RenderCopyEx(renderer, texture, null, null, 0, null, @intToEnum(c.SDL_RendererFlip, c.SDL_FLIP_VERTICAL));
-        _ = c.SDL_RenderPresent(renderer);
+    while (app.running) {
+        app.handleEvents();
+        app.draw();
     }
 
-    _ = c.FreeImage_Save(image.format, image.bitmap, "output_image", 0);
+    app.saveImage();
 }
